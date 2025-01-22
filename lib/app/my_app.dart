ffcdd52049ab86afdaa8eab99f4e76f00e5fd55c
Diego Castro <omegadiegocastro@gmail.com>
2025-01-22 14:16:46 -0300

    feat(app): compile android version - 34

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app.dart';
 
@@ -9,15 +8,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: const [],
-      child: MaterialApp(
-        //Todo: Rename App
-        title: 'MyApp',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeConfig.theme,
-        home: const HomePage(),
-      ),
+    return MaterialApp(
+      //Todo: Rename App
+      title: 'MyApp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeConfig.theme,
+      home: const HomePage(),
     );
   }
 }
